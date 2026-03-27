1d58a344411d776e7827f3c5206db7356aed5e21
chaitanyateja13 <chaitanyateja1318@gmail.com>
2026-03-27 09:52:04 +0530

    UC11: Regex validation

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,31 +1,18 @@
-import java.util.*;
+import java.util.regex.*;
 
 public class TrainConsistManagementApp {
 
-    static class Bogie {
-        String name;
-        int capacity;
-
-        Bogie(String name, int capacity) {
-            this.name = name;
-            this.capacity = capacity;
-        }
-    }
-
     public static void main(String[] args) {
 
-        System.out.println("=== UC10: Total Capacity ===");
+        System.out.println("=== UC11: Regex Validation ===");
 
-        List<Bogie> bogies = List.of(
-                new Bogie("Sleeper", 72),
-                new Bogie("AC", 60),
-                new Bogie("First", 30)
-        );
+        String trainId = "TRN-1234";
+        String cargo = "PET-AB";
 
-        int total = bogies.stream()
-                .map(b -> b.capacity)
-                .reduce(0, Integer::sum);
+        Pattern trainPattern = Pattern.compile("TRN-\\d{4}");
+        Pattern cargoPattern = Pattern.compile("PET-[A-Z]{2}");
 
-        System.out.println("Total Capacity: " + total);
+        System.out.println("Train Valid: " + trainPattern.matcher(trainId).matches());
+        System.out.println("Cargo Valid: " + cargoPattern.matcher(cargo).matches());
     }
 }
